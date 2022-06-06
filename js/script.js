f352f1b36a419177c06673c58de879a8dd34b2c7
Patricio Gonzalez <Razer81368@gmail.com>
2022-06-06 05:27:19 -0400

    portfolio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector(".nav"),
+  navlist = nav.querySelectorAll("li"),
+  totalNavList = navlist.length;
+for (let i = 0; i < totalNavList; i++) {
+  const a = navlist[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let j = 0; j < totalNavList; j++) {
+      navlist[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
